1d49b0fa37f41240feb9b2bff3772388f922e47c
zhaokai <zhaokai.work@foxmail.com>
2021-08-16 10:13:31 +0800

    feat: 一些历史积攒的项目按文件夹目录区分

---
+++ Plugin/PluginLoadBaseMap/main.cpp
@@ -0,0 +1,20 @@
+﻿#include "MainWindow.h"
+
+#include <QApplication>
+#include <QtPlugin>
+
+//Q_IMPORT_PLUGIN(CustomBaseMapPlugin)
+
+int main(int argc, char *argv[])
+{
+
+    QApplication a(argc, argv);
+    QCoreApplication::setApplicationName("LoadBaseMap");
+    QCoreApplication::setOrganizationName("XiuShan");
+    QCoreApplication::setOrganizationDomain("supersurs.com");
+    MainWindow w;
+    w.show();
+
+    qDebug() << QImageReader::supportedImageFormats();
+    return a.exec();
+}
