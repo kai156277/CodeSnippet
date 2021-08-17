b1124c5a243cc9772ff7e7cd55fb7f0749ecebc6
zhaokai <zhaokai.work@foxmail.com>
2021-08-17 11:00:24 +0800

    feat: Qt UI 界面练习代码

---
+++ samp2_1/Widget.cpp
@@ -0,0 +1,33 @@
+﻿#include "Widget.h"
+#include "ui_Widget.h"
+
+#include <QFileDialog>
+#include <QMessageBox>
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+    connect(ui->pushButton_openFile, &QPushButton::clicked, this, &Widget::when_pushButton_openFile_clicked);
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+//void Widget::on_pushButton_openFile_clicked()
+//{
+//}
+
+void Widget::when_pushButton_openFile_clicked()
+{
+    QString fileName = QFileDialog::getOpenFileName(this, tr("Open File"), "/home", tr("Images (*.png *.xpm *.jpg);;Text files (*.txt);;XML files (*.xml)"));
+    if (fileName.isEmpty())
+    {
+        QMessageBox::warning(this, "warning", " open a empty file");
+        return;
+    }
+    ui->lineEdit_openFile->setText(fileName);
+}
---
+++ samp2_1/Widget.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+#include <QWidget>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class Widget;
+}
+QT_END_NAMESPACE
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Widget(QWidget *parent = nullptr);
+    ~Widget();
+
+private slots:
+    //    void on_pushButton_openFile_clicked();
+    void when_pushButton_openFile_clicked();
+
+private:
+    Ui::Widget *ui;
+};
